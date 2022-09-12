91e27a1752fcf05a2e22994255a246dd7ac03231
ivan.aksionau <ivanionov2015@mail.ru>
2022-09-12 14:41:54 +0300

    add Flyweight pattern

--- src/main/java/com/ivan/practice/pattern/structuraldesign/flyweight/Client.java
+++ src/main/java/com/ivan/practice/pattern/structuraldesign/flyweight/Client.java
@@ -1,6 +1,13 @@
 package com.ivan.practice.pattern.structuraldesign.flyweight;
 
 /**
+ * We use flyweight design pattern if we need large number of objects of class where
+ * we can easily separate out state that can be shared and state that can be externalized.
+ * <p>
+ * Flyweights store only "intrinsic" state or state that can be shared in any context.
+ * <p>
+ * Code using flyweight instance provides the extrinsic state when calling methods on flyweight.
+ * Flyweight object then uses this state along with its inner state to carry out the work.
  * java.lang.Integer is an example of flyweight pattern
  */
 public class Client {
